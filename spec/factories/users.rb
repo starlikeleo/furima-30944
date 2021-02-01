41414a25699b134d4ff6607095525b75100c802f
starlikeleo <73845797+starlikeleo@users.noreply.github.com>
2021-02-01 12:06:28 +0900

    テストコード実装２_commit

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
   factory :user do
-    Faker::Config.locale = :en
+    transient do
+      person { Gimei.name }
+    end
+
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password }
+    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
     password_confirmation { password }
-    Faker::Config.locale = :ja
-    family_name           { Faker::Name.last_name }
-    first_name            { Faker::Name.first_name }
-    kana_family_name      { Faker::Name.last_name }
-    kana_first_name       { Faker::Name.first_name }
+    family_name           { person.last.kanji }
+    first_name            { person.first.kanji }
+    kana_family_name      { person.last.katakana }
+    kana_first_name       { person.first.katakana }
     birthday              { Faker::Date.birthday }
   end
 end
---
+++ spec/requests/items_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe ItemsController, type: :request do
+
+  describe "GET #index" do
+    it "indexアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get root_path
+
+    end
+    it "indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する" do 
+    end
+    it "indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する" do 
+    end
+    it "indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する" do 
+    end
+  end
+end
