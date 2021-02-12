25c4caa4c9c07cca0d49c9469cde2701a8b4fa88
starlikeleo <73845797+starlikeleo@users.noreply.github.com>
2021-02-12 18:09:36 +0900

    商品出品_jsp_commit

--- app/javascript/channels/item_price.js
+++ app/javascript/channels/item_price.js
@@ -1,3 +1,14 @@
 window.addEventListener('load', () => {
-  console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+
+    const inputValue = priceInput.value;
+    console.log(inputValue);
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = inputValue * 0.1
+
+    const addProfit = document.getElementById("profit");
+    addProfit.innerHTML = inputValue - (inputValue * 0.1)
+  })
 });
