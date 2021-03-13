962b265624855c89a5c6c86284bbfbebad536864
starlikeleo <73845797+starlikeleo@users.noreply.github.com>
2021-03-14 00:31:42 +0900

    five_commit

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,6 +2,7 @@ class  BuysController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
 
   def index
+    @buy_destination = BuyDestination.index(destination_params)
   end
 
   def new
@@ -16,13 +17,12 @@ class  BuysController < ApplicationController
       else
         redirect_to action: :index
       end
-    end
   end
 
   private
    # 全てのストロングパラメーターを1つに統合
   def destination_params
-    params.require(:buy_destination).permit(:postal_code, :delivery_area_id, :municipal_name, :house_number, :building_name, :tell).
-    merge(user_id: params[:user_id], item_id: params[:item_id])
+    params.require(:buy_destination).permit(:postal_code, :delivery_area_id, :municipal_name, :house_number, :building_name, :tell).merge(user_id: params[:user_id], item_id: params[:item_id])
   end
+
 end
